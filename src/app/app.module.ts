2ef270b609d22f5e72ccb7a07f00a4b059d1a28a
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-02-11 10:32:37 -0300

    feat(module): modify app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardModule  } from 'primeng/card'
+import { InputTextModule } from 'primeng/inputtext'
+import { ButtonModule } from 'primeng/button'
+import { ToastModule } from 'primeng/toast'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
+import {CookieService} from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: // Components que pertenceram a esse módulo...
+  [
+    AppComponent,
+    HomeComponent
   ],
-  imports: [
+  imports: // Importação de outros modulos ou modulos do próprio angular...
+  [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule, // Para trabalhar com requisições HTTP
+    // PRIME NG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: // Serviços para prover dentro do múdulo.
+  [CookieService, MessageService], // Cookie
+  bootstrap: [AppComponent], // Apenas para o app.module
+  // exports: []// caso eu queira exportar algum component
 })
 export class AppModule { }
